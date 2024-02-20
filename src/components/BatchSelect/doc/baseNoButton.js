75eefeec75b31c7c3cae16bdbf26af06a3cf0a58
jinfanghuang <2283785225@qq.com>
2024-02-20 17:29:35 +0800

    feat: add init form data

--- src/components/BatchSelect/doc/baseNoButton.js
+++ src/components/BatchSelect/doc/baseNoButton.js
@@ -1,27 +1,19 @@
 const { default: BatchSelect } = _BatchSelect;
 const { createWithRemoteLoader } = remoteLoader;
 
+const initFormData = {
+  candidate: [
+    { id: '1', name: '候选人1', position: '职位1' },
+    { id: '2', name: '候选人2', position: '职位2' }
+  ]
+};
+
 const BaseExample = createWithRemoteLoader({
   modules: ['components-core:FormInfo@Form']
 })(({ remoteModules }) => {
   const [Form] = remoteModules;
   return (
-    <Form
-      data={{
-        candidate: [
-          {
-            id: '1',
-            name: '候选人1',
-            position: '职位1'
-          },
-          {
-            id: '2',
-            name: '候选人2',
-            position: '职位2'
-          }
-        ]
-      }}
-    >
+    <Form data={initFormData}>
       <BatchSelect
         labelRender={({ label, value }) => {
           return `${label}:${(value && value.length) || 0}人`;
